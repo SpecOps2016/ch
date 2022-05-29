8b420b58170f95ee3cbae0cb11e0816c1ebce097
SpecOps2016 <gregory.wong@ymail.com>
2022-05-30 00:42:38 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
 import './App.css';
 import Login from "./Login";
